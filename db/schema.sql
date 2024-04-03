906e6033ffb47ef0cc32565d641778af8921753c
Daniel John <dcampo85@gmail.com>
2024-04-03 12:50:09 -0400

    db and schema

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+USE organization;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS positions;
+
+CREATE TABLE departments (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE positions (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL
+  salary DECIMAL NOT NULL
+  department_id INTEGER,
+  CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  position_id INTEGER,
+  manager_id INTEGER,
+  CONSTRAINT fk_position_id FOREIGN KEY (position_id) REFERENCES positions(id),
+  CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
