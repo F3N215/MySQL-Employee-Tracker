75e4fb626b1a14ea518371a8dc1993bfb837ed16
Daniel John <dcampo85@gmail.com>
2024-04-08 12:00:19 -0400

    tutor sesh 2

--- db/connection.js
+++ db/connection.js
@@ -3,14 +3,19 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 // creates a connection to the database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  },
-  console.log("You connected to the database! Way to go!")
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("There was an error " + err.stack);
+    return;
+  }
+  console.log("You connected to the database! Way to go!");
+});
 
 module.exports = db;
